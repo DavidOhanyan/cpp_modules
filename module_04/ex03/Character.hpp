3dcd2e5fea7a00f1dce1878b8c832d7dfd47ba81
David_Ohanyan <davitoahnyan630@gmail.com>
2023-11-08 22:26:47 +0400

    Character half

--- module_04/ex03/Character.hpp
+++ module_04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 22:11:40 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/11/06 22:20:12 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/11/08 21:38:10 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,16 @@
 
 class Character: public ICharacter
 {
-	
+private:
+	std::string name;
+	AMateria *slots[4];
+public:
+	Character();
+	Character(const )
+	std::string const & getName() const;
+	void equip(AMateria* m);
+	void unequip(int idx);
+	void use(int idx, ICharacter& target);
 };
 
 #endif
--- module_04/ex03/main.cpp
+++ module_04/ex03/main.cpp
@@ -6,14 +6,21 @@
 /*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 18:38:05 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/11/05 18:38:24 by dohanyan         ###   ########.fr       */
+/*   Updated: 2023/11/08 20:51:56 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
 
-int main()
+void lol(int *a)
 {
+	std::cout<<"adddres in function = " << a << std::endl;
+}
 
+int main()
+{	
+	int *a = (int *)malloc(sizeof(int));
+	std::cout<<"in main address = "<<a<<std::endl;
+	lol(a);
 	return (0);
 }
