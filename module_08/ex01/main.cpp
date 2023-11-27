d0f2ccd6c2028df3b1772fcea09fbc499db0524e
Davit Ohanyan <dohanyan@c2r3s5.42yerevan.am>
2023-11-27 18:18:54 +0400

    ready for evaluation

--- module_08/ex01/main.cpp
+++ module_08/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 17:29:20 by tumolabsstu       #+#    #+#             */
-/*   Updated: 2023/11/26 01:46:49 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/27 13:20:42 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int main()
 {
 	try
 	{
-		Span sp = Span(5);
+		Span sp = Span(7);
 		
 		sp.addNumber(6);
 		sp.addNumber(3);
 		sp.addNumber(17);
 		sp.addNumber(9);
 		sp.addNumber(11);//3 6 9 11 17
+		sp.addNumber(88);
+		sp.addNumber(89);
 
 		std::cout<< sp.shortestSpan() <<std::endl;
 		std::cout<< sp.longestSpan() <<std::endl;
--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -11,10 +11,37 @@
 /* ************************************************************************** */
 
 #ifndef _MUTANTSTACK_HPP_
-#define MUTANTSTACK_HPP_
+#define _MUTANTSTACK_HPP_
 
 #include <iostream>
+#include <stack>
+
+template<class Tp, class Container = std::deque<Tp> >
+class MutantStack:public std::stack<Tp, Container>
+{
+public:
+    MutantStack(){}
+    MutantStack(const MutantStack<Tp, Container>& other):std::stack<Tp, Container>(other){}
+    MutantStack& operator=(const MutantStack<Tp, Container>& other){if (this != &other){std::stack<Tp, Container>::operator=(other);} return (*this);}
+    ~MutantStack(){}
+
+    typedef typename std::stack<Tp, Container>::container_type::iterator iterator;
+    typedef typename std::stack<Tp, Container>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<Tp, Container>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<Tp, Container>::container_type::const_reverse_iterator const_reverse_iterator;
 
 
+    iterator begin(){return (this->c.begin());}
+    iterator end(){return (this->c.end());}
+
+    const_iterator cbegin(){return (this->c.begin());}
+    const_iterator cend(){return (this->c.end());}
+
+    reverse_iterator rbegin(){return (this->c.rbegin());}
+    reverse_iterator rend(){return (this->c.rend());}
+
+    const_reverse_iterator crbegin(){return (this->c.crbegin);}
+    const_reverse_iterator crend(){return (this->c.crend);}
+};
 
 #endif
