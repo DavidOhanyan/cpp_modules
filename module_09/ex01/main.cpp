99e3790e08f56368ae880ed2f84624dfcf11afe7
Davit Ohanyan <dohanyan@c2r3s3.42yerevan.am>
2023-12-19 16:49:43 +0400

    fix seg

--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dohanyan <dohanyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:02:59 by dohanyan          #+#    #+#             */
-/*   Updated: 2023/12/10 15:54:48 by marvin           ###   ########.fr       */
+/*   Updated: 2023/12/19 15:09:43 by dohanyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main(int argc, char **argv)
 	{
 		if (argc != 2)
 			throw std::runtime_error(ERR_BAD_ARG);
+		if (std::string(argv[1]).find_first_not_of(' ') == std::string::npos)
+			throw std::runtime_error(ERR_BAD_INPUT);
 		RPN::pars(argv[1]);
 
 	}
